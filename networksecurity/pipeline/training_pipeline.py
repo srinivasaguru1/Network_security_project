8e22cf4087f19b25210b8c52232e8a1e377bd69b
srinivasaguru1 <srinivasaguru301@gmail.com>
2024-12-21 23:48:55 +0530

    config and artifact is ready

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -31,11 +31,19 @@ from networksecurity.entity.artifact_entity import (
 )
 
 class TrainingPipeline:
+    is_pipeline_running=False
     def __init__(self):
-        pass
+         self.training_pipeline_config = TrainingPipelineConfig()
+         self.s3_sync = S3Sync()
     def start_data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+            data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
+            
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
