d89318a15664a10b06c67f257bcde2b1496d30ef
srinivasaguru1 <srinivasaguru301@gmail.com>
2024-12-24 15:36:57 +0530

    model trainer code is ready and main component needed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -57,7 +57,11 @@ DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 """
 Model Trainer related constant start with MODEL_TRAINER VAR NAME
 """
-
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
 """
 
 Model Evaluation related constant start with MODEL_EVALUATION VAR NAME
