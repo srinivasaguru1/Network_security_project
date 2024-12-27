3b541bcd571601be3fb231fd8041c3b1e02c737b
srinivasaguru1 <srinivasaguru301@gmail.com>
2024-12-27 14:44:02 +0530

    docker updated

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -29,12 +29,14 @@ from networksecurity.entity.artifact_entity import (
     ModelEvaluationArtifact,
     ModelPusherArtifact    
 )
-
+from networksecurity.cloud.s3_syncer import S3Sync
+from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
+from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR
 class TrainingPipeline:
     is_pipeline_running=False
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
-         
+        self.s3_sync = S3Sync()
     def start_data_ingestion(self):
         try:
             self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
